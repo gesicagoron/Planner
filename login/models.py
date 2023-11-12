ee5acdddf3fd0a77fb772c0b80d37caf8af08ab6
Andreea <andreea.gab.and@gmail.com>
2023-11-13 00:06:50 +0200

    legare baza date cu intrerfata, login, logout, register

--- login/models.py
+++ login/models.py
@@ -1,27 +1,3 @@
 from django.db import models
-from django.utils import timezone
-from django.contrib.auth.models import User
 
-
-class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
-    
-class CustomUser(models.Model):
-    username = models.CharField(max_length=50, unique=True)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
-    first_name = models.CharField(max_length=45)
-    last_name = models.CharField(max_length=45)
-
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-
-    def __str__(self):
-        return self.username
+# Create your models here.
--- login/views.py
+++ login/views.py
@@ -1,40 +1,9 @@
 from django.shortcuts import render
-from .models import Post
-
-<<<<<<< HEAD
-
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'login/home.html', context)
-
-=======
-# Create your views here.
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
-
+from django.http import HttpResponse
 
 
 def home(request):
-    context = {
-        'posts': posts
-    }
-
-    return render(request, 'login/home.html',context)
->>>>>>> 5090acacecee90c49bd8be207879b860ed4c97c7
+    return render(request, 'login/home.html')
 
 def about(request):
-    return render(request, 'login/about.html', {'title': 'About'})
+    return HttpResponse('<h1>About</h1>')
