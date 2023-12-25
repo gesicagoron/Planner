88b2a99391d4eee5cb5fc787f5a5312354e72878
c-andreea <92252346+c-andreea@users.noreply.github.com>
2023-12-25 20:07:09 +0200

    Update views.py pentru ca se corupse

--- users/views.py
+++ users/views.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -21,8 +20,3 @@ def register(request):
 @login_required
 def profile(request):
     return render(request, 'users/profile.html')
-=======
-from django.shortcuts import render
-
-# Create your views here.
->>>>>>> ee5acdddf3fd0a77fb772c0b80d37caf8af08ab6
