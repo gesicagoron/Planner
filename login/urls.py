8fcfd5ac8a55e4044b0bed49bc684be129535f70
c-andreea <92252346+c-andreea@users.noreply.github.com>
2023-12-25 20:09:12 +0200

    Update urls.py se corupse

--- login/urls.py
+++ login/urls.py
@@ -1,14 +1,8 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
+from .import views
 
 urlpatterns = [
-<<<<<<< HEAD
     path('', views.home, name='login'),
     path('about/', views.about, name='login-about'),
-  
 ]
-=======
-    path('', views.home,name='login-home'),
-    path('about/', views.about,name='login-about'),
-]
->>>>>>> ee5acdddf3fd0a77fb772c0b80d37caf8af08ab6
