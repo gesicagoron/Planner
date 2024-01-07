2970f2bfce329bdae2e7b71c02870aff07f2a751
Andreea <andreea.gab.and@gmail.com>
2024-01-07 17:16:36 +0200

    comentarii la postari

--- login/models.py
+++ login/models.py
@@ -3,18 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
-class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
 
-    def __str__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.pk})
     
 class CustomUser(models.Model):
     username = models.CharField(max_length=50, unique=True)
@@ -30,3 +19,24 @@ class CustomUser(models.Model):
     def __str__(self):
         return self.username
 
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.author.username} - {self.post.title}'
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='post_likes', blank=True)
+    comments = models.ManyToManyField(Comment, related_name='post_comments', blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
--- login/views.py
+++ login/views.py
@@ -7,9 +7,14 @@ from django.views.generic import (
     UpdateView,
     DeleteView
 )
-from .models import Post
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+from .models import Post, Comment
 from django.shortcuts import get_object_or_404
 
+from django.urls import reverse
+from django.urls import reverse_lazy
 def home(request):
     context = {
         # You can add any additional context data for the home page here
@@ -50,10 +55,57 @@ class PostDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         post = self.get_object()
         context['like_count'] = post.likes.count()
+        context['comments'] = Comment.objects.filter(post=post)
         return context
+    
+
+# Add a new view for handling comments
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    fields = ['content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.post = Post.objects.get(pk=self.kwargs['pk'])
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        # Redirect to the post detail page after successful form submission
+        return reverse('post-detail', kwargs={'pk': self.kwargs['pk']})
+# Modify your like_post view to include redirecting back to the post detail page
+def like_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.user in post.likes.all():
+        post.likes.remove(request.user)
+    else:
+        post.likes.add(request.user)
+    return redirect('post-detail', pk=pk)
+
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    fields = ['content']
+    template_name = 'login/comment_form.html'  # Create a new template for comment editing
+
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.post.pk})
 
 
+@login_required
+def comment_delete(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    
+    # Check if the user has permission to delete the comment
+    if request.user == comment.author:
+        comment.delete()
+        messages.success(request, 'Comment deleted successfully.')
+    else:
+        messages.error(request, 'You do not have permission to delete this comment.')
 
+    return redirect('post-detail', pk=comment.post.pk)
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
