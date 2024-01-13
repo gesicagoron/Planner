c7c48164508e6a0e4e119e4a084556e6cac522b5
Andreea <andreea.gab.and@gmail.com>
2024-01-14 01:08:34 +0200

    calendar

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Profile
 from .models import Task
-
+from .models import Event
 admin.site.register(Profile)
 admin.site.register(Task)
+admin.site.register(Event)
 # Register your models here.
