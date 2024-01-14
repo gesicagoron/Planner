d6e39122909fa75caa18de1e5b6c502e0de1f894
gesicagoron <126571250+gesicagoron@users.noreply.github.com>
2024-01-14 18:29:17 +0200

    functionalitati itinerary si to do list, design pattern observer

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import TaskListView, TaskCreate, TaskUpdate, DeleteView
+from .views import ItineraryListView, ItineraryCreate, ItineraryUpdate
 from . import views
 
 urlpatterns = [
@@ -7,4 +8,8 @@ urlpatterns = [
      path('task-create/', TaskCreate.as_view(), name='task-create'),
      path('task-update/<int:pk>', TaskUpdate.as_view(), name='task-update'),
      path('task-delete/<int:pk>', DeleteView.as_view(), name='task-delete'),
+     path('task_list/', ItineraryListView.as_view(), name='itinerary_list'),
+     path('itinerary-create/', ItineraryCreate.as_view(), name='itinerary-create'),
+     path('itinerary-update/', ItineraryUpdate.as_view(), name='itinerary-update'),
+   
 ]
