262e1ca1ab06a3c25560c104bf463229983ce38b
Lodroman Denisa-Georgiana <lodroman.denisa@gmail.com>
2024-01-14 04:05:09 +0200

    Revizuire

--- users/models.py
+++ users/models.py
@@ -33,3 +33,13 @@ class Task(models.Model):
     
     class Meta:
         ordering = ['complete']
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()  
+    @property
+    def get_html_url(self):
+        url = reverse('users:event_edit', args=(self.id,))
+        return f'<a href="{url}"> {self.title} </a>'      
--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from .views import TaskListView, TaskCreate, TaskUpdate, DeleteView
 from . import views
+from django.urls import re_path
 
+app_name = 'users'
 urlpatterns = [
      path('task_list/', TaskListView.as_view(), name='task_list'),
      path('task-create/', TaskCreate.as_view(), name='task-create'),
      path('task-update/<int:pk>', TaskUpdate.as_view(), name='task-update'),
      path('task-delete/<int:pk>', DeleteView.as_view(), name='task-delete'),
+     path('calendar/', views.CalendarView.as_view(), name='calendar'),
+     path('event/new/', views.event, name='event_new'),
+      re_path(r'^event/edit/(?P<event_id>\d+)/$', views.event, name='event_edit'),
 ]
